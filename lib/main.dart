b99b855590d0da6cdc830628dd3cbd76dc106da0
Islam Ahmad <islam.ahmad@almtar.com>
2019-01-27 05:57:08 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:sqflite_app/ui/Home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'learning sqflite',
+    home: Home(),
+  ));
+}
