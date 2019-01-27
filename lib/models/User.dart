cc32f678c16bd778175e4c887b7468f04294e45d
Islam Ahmad <islam.ahmad@almtar.com>
2019-01-27 07:44:55 +0200

    created two methods for user class to create user from map, and to create map from user

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -3,16 +3,21 @@ class User {
   String _pass;
   int _id;
   User(this._name, this._pass);
-
   User.map(dynamic obj) {
     this._name = obj["name"];
     this._pass = obj["pass"];
     this._id = obj["id"];
   }
+  User.fromMap(Map<String, dynamic> map) {
+    this._name = map["name"];
+    this._pass = map["pass"];
+    if (map["id"] != null) {
+      this._id = map["id"];
+    }
+  }
   String get name => _name;
   int get id => _id;
   String get pass => _pass;
-
   set name(String value) {
     _name = value;
   }
@@ -23,5 +28,15 @@ class User {
 
   set id(int value) {
     _id = value;
+  }
+
+  Map<String, dynamic> toMap() {
+    Map map = Map<String, dynamic>();
+    map["name"] = _name;
+    map["pass"] = _pass;
+    if (id != null) {
+      map["id"] = _id;
+    }
+    return map;
   }
 }
