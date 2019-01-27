babc7b39ce0935d1288b35c882a5c79c1e823da9
Islam Ahmad <islam.ahmad@almtar.com>
2019-01-27 06:32:52 +0200

    added path provider in pubspec

--- lib/ui/Home.dart
+++ lib/ui/Home.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+//import 'package:sqflite/sql.dart';
+//import 'package:sqflite/sqflite.dart';
 
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.redAccent,
+        title: Text(
+          'SQFLite',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          Center(
+            child: Text('Text'),
+          )
+        ],
+      ),
+    );
   }
 }
+
+//class Home extends StatefulWidget {
+//  @override
+//  _HomeState createState() => _HomeState();
+//}
+//
+//class _HomeState extends State<Home> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold();
+//  }
+//}
